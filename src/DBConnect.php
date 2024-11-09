e3efe8383189a960766d288d88e516d085921b14
Hendro CPL <endrocpl@gmail.com>
2024-11-09 15:06:13 +0700

    Update DBConnect.php

--- src/DBConnect.php
+++ src/DBConnect.php
@@ -7,11 +7,6 @@ class DBConnect
 
     function __construct()
     {
-        // $this->connect = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-        // if (mysqli_connect_errno($this->connect)) {
-        //     echo 'failed to connect database' . mysqli_connect_error();
-        // }
-        //  $this->connect = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
         $this->connect = pg_connect("host=" . DB_HOST . " port=" . DB_PORT . " dbname=" . DB_DATABASE . " user=" . DB_USER . " password=" . DB_PASSWORD);
     }
     public function getDB()
